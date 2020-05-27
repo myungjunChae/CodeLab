ecf5e71cd994f10378575fe00147762122210bac
MyungjunChae <cmj9597@naver.com>
2020-05-27 13:48:00 +0900

    About ViewModel, ViewModelFactory

--- GuessTheWord-Starter/app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModelFactory.kt
+++ GuessTheWord-Starter/app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModelFactory.kt
@@ -1,2 +1,15 @@
 package com.example.android.guesstheword.screens.score
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+import java.lang.IllegalArgumentException
+
+class ScoreViewModelFactory(private val score : Int) : ViewModelProvider.Factory{
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if(modelClass.isAssignableFrom(ScoreViewModel::class.java)){
+            return ScoreViewModel(score) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel Class")
+    }
+}
